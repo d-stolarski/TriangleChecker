9d95f2742ab278bba3602ff284023dafc4fb6563
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-05 14:51:58 +0100

    final commit

--- src/logic/TriangleChecker.java
+++ src/logic/TriangleChecker.java
@@ -4,14 +4,32 @@ import data.Triangle;
 
 public class TriangleChecker {
 
-    public boolean isRightTriangle(Triangle triangle){
-        boolean score = false;
-        if(triangle.getDimensionA() * triangle.getDimensionA() + (triangle.getDimensionB() * triangle.getDimensionB())
-                == triangle.getDimensionC() * triangle.getDimensionC()) {
-            score = true;
+    public boolean isRightTriangle(Triangle triangle) {
+
+        double theGreatestDimension;
+        double shorterDimension1;
+        double shorterDimension2;
+
+        if (triangle.getDimensionA() > triangle.getDimensionB() && triangle.getDimensionA() > triangle.getDimensionC()) {
+            theGreatestDimension = triangle.getDimensionA();
+            shorterDimension1 = triangle.getDimensionB();
+            shorterDimension2 = triangle.getDimensionC();
+            return Math.pow(shorterDimension1, 2) + Math.pow(shorterDimension2, 2) == Math.pow(theGreatestDimension, 2);
+
+        } else if (triangle.getDimensionB() > triangle.getDimensionA() && triangle.getDimensionB() > triangle.getDimensionC()) {
+            theGreatestDimension = triangle.getDimensionB();
+            shorterDimension1 = triangle.getDimensionA();
+            shorterDimension2 = triangle.getDimensionC();
+            return Math.pow(shorterDimension1, 2) + Math.pow(shorterDimension2, 2) == Math.pow(theGreatestDimension, 2);
+
         } else {
-            score = false;
+            theGreatestDimension = triangle.getDimensionC();
+            shorterDimension1 = triangle.getDimensionA();
+            shorterDimension2 = triangle.getDimensionB();
+            return Math.pow(shorterDimension1, 2) + Math.pow(shorterDimension2, 2) == Math.pow(theGreatestDimension, 2);
         }
-        return score;
+
     }
+
+
 }
